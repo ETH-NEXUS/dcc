f2e136eda5ab25b7224157b95af72e4c52d8a743
dameyerdave <dameyerdave@gmail.com>
2024-07-01 08:44:07 +0200

    [FEAT] more talkative setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
 url=$(curl -s https://api.github.com/repos/ETH-NEXUS/dcc/releases/latest | grep browser_download_url | cut -d '"' -f 4)
+echo "Downloading latest version of dcc from ${url}..."
 curl -s -L -o dcc.tar.gz ${url}
 
+echo "Extracting package..."
 tar xzf dcc.tar.gz
 
+target=${1:-~/.local/bin}
+echo "Installing to ${target}"
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    sudo cp dist/linux/dcc /usr/bin
+    sudo cp dist/linux/dcc ${target}
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    sudo cp dist/mac/dcc ${1:-/usr/bin}
+    sudo cp dist/mac/dcc ${target}
 fi
 
+echo "Cleaning up..."
 rm -rf dist dcc.tar.gz
+
+echo "DONE."
